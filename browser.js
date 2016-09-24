4af22d9bde627c5ae1ba3723bc5c5734fd3f5cbe
Gertt <gert@gert.gert>
2016-09-24 15:07:50 +0200

    fix unicode hack to be compliant with real base64 [resolves #1]

--- browser.js
+++ browser.js
@@ -1,17 +1,21 @@
 'use strict';
 
 /**
- * Note on escape and encode functions:
+ * Note on encode and replace functions:
  * These is for Unicode support
- * https://developer.mozilla.org/en-US/docs/Web/API/WindowBase64/btoa#Unicode_strings
+ * https://developer.mozilla.org/en-US/docs/Web/API/WindowBase64/Base64_encoding_and_decoding#The_Unicode_Problem
  */
 
 var encode = function (source) {
-    return btoa(escape(encodeURIComponent(source)));
+    return btoa(encodeURIComponent(source).replace(/%([0-9A-F]{2})/g, function(m, p1) {
+        return String.fromCharCode('0x' + p1);
+    }));
 };
 
 var decode = function (encoded) {
-    return decodeURIComponent(unescape(atob(encoded)));
+    return decodeURIComponent(Array.prototype.map.call(atob(encoded), function(c) {
+        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+    }).join(''));
 };
 
 module.exports = require('./common')(encode, decode);
